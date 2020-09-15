a0c4604590aad22665581e2936ce7a2e1cb6c646
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-09-15 19:16:27 -0400

    add - addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,22 @@
 function addBorder(picture: string[]): string[] {
+  const lengthOfWall = picture[0].length + 2;
 
+  const wall = '*'.repeat(lengthOfWall);
+
+  console.log(wall);
+  console.log(picture);
+
+  picture.unshift(wall); // ?
+  picture.push(wall); // ?
+
+  picture; // ?
+  picture.length; // ?
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i]; // ?
+    picture[i] = '*'.concat(picture[i], '*'); // ?
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']));
